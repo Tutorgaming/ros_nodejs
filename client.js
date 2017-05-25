6b4e15dbc8ffc906dc7379065cc478a70ff22af1
Theppasith Nisitsukcharoen <tutorgaming@iTUTORs-MacBook-Air.local>
2017-05-25 18:35:19 +0700

    Edit Client and Index(server)

--- client.js
+++ client.js
@@ -1,65 +1,85 @@
 /*
     HGMC UDP RELAY
-    Description : A Relay to be the UDP link from Mission planner 
+    Description : A Relay to be the UDP link from Mission planner
                   to the socket.io link ( to the logging and mailer server )
-    Expectation : QT can do this function as well 
-    Author : Theppasith Nisitsukcharoen 
+    Expectation : QT can do this function as well
+    Author : Theppasith Nisitsukcharoen
 */
 import io from 'socket.io-client'
 import mavlink from 'mavlink'
-import BN from 'bn.js'
+import amqp from 'amqplib/callback_api'
+import amqp2 from 'amqplib/callback_api'
+import udp from 'dgram'
 
-// MAVLink Parser (for conversion between ROSMAV - MAV)
-const MAVParser = new mavlink(1,190);
+const RECEIVE_QUEUE_NAME = "robot_receive_queue";
+const ROBOT_QUEUE_NAME = "to_robot_queue";
 
-// import rosnodejs from './rosnodejs/index.js'
-// import Deserialize from './rosnodejs/ros_msg_utils/index.js'
-// socket-io client
-const SOCKET_IO_SERVER  = 'http://192.168.1.19:3000'
-const socket            = io.connect(SOCKET_IO_SERVER)
+// // socket-io client const SOCKET_IO_SERVER  = 'http://192.168.1.19:3000'
+// const socket            = io.connect(SOCKET_IO_SERVER) Create UDP Client for
+// HGMC(server)
+const TO_HGMC_UDP_PORT = 14550;
+const TO_HGMC_UDP_IP = "192.168.1.19";
 
-// Create UDP Client for HGMC(server) 
-const TO_HGMC_UDP_PORT  = 14550;
-const TO_HGMC_UDP_IP    = "192.168.1.19";
+// Create UDP Client
+var client = udp.createSocket('udp4');
 
-// var client = dgram.createSocket('udp4');
-let payload64 = [
-    new BN("39af6ecc3", 'hex'),
-    new BN("fffff934ffffec3b", 'hex'),
-    new BN("ff00ff00000000", 'hex'),
-    new BN("ff0300ff00ff", 'hex')
-];
-
-console.log(payload64)
-
-let count = 0;
-let payload = payload64.reduce(
-    (last,cur)=>
-        Buffer.concat([last,cur.toBuffer('le',8)])
-    ,
-    new Buffer(0)
-);
-
-console.log(payload);
-
-MAVParser.on('ready', ()=>{
-    console.log("MAVParser Ready");
-    MAVParser.createMessage("GPS_STATUS",
-		{
-		'satellites_visible':		5,
-		'satellite_prn':			[1, 2, 3, 4, 5],
-		'satellite_used':			[2, 3, 4, 5, 6],
-		'satellite_elevation':		[3, 4, 5, 6, 7],
-		'satellite_azimuth':		[4, 5, 6, 7, 8],
-		'satellite_snr':			[5, 6, 7, 8, 9]
-		},
-		function(message) {
-			console.log(message.buffer.length);
-		});
+// AMQP Instance Connect
+// connects to rabbitmq
+amqp.connect('amqp://localhost', function(err, conn) {
+    if (err != null) bail(err); // calls `bail` function if an error occurred when connecting
+    //consumer(conn); // creates a consumer
+    //publisher(conn); // creates a publisher
 });
 
-    
+amqp2.connect('amqp://localhost', function(err, conn) {
+    if (err != null) bail(err); // calls `bail` function if an error occurred when connecting
+    consumer(conn); // creates a consumer
+    //publisher(conn); // creates a publisher
+});
 
-// Consume AMQP
+function bail(err) {
+    console.error(err);
+    process.exit(1);
+}
 
-// Receive Value back from HGMC,QGC,Mission planner 
+// Publisher
+function publisher(conn) {
+    conn.createChannel(on_open); // creates a channel and call `on_open` when done
+    function on_open(err, ch) {
+        if (err != null) bail(err); // calls `bail` function if an error occurred when creating the channel
+        ch.assertQueue(ROBOT_QUEUE_NAME, {
+            durable: false
+        });// asserts the queue exists
+        client.on('message', function (msg, info) {
+            console.log(msg);
+           ch.sendToQueue(ROBOT_QUEUE_NAME, msg); // sends a message to the queue
+        });
+        // ch.sendToQueue(q, new Buffer('something to do')); // sends a message to the queue
+    }
+}
+
+// Consumer
+function consumer(conn) {
+    var ok = conn.createChannel(on_open); // creates a channel and call `on_open` when done
+    function on_open(err, ch) {
+
+        if (err != null) bail(err); // calls `bail` function if an error occurred when creating the channel
+        ch.assertQueue(RECEIVE_QUEUE_NAME, {
+            durable: false
+        });
+        ch.consume(RECEIVE_QUEUE_NAME, function(msg) { //consumes the queue
+            //console.log(msg.content);
+            if (msg !== null) {
+                client.send(msg.content,14550,TO_HGMC_UDP_IP,function(){
+                    //console.log("PUSH TO HGMC");
+                })
+            }
+        });
+
+        client.on('message', function (msg, info) {
+            console.log(msg);
+           ch.sendToQueue(ROBOT_QUEUE_NAME, msg); // sends a message to the queue
+        });
+    }
+}
+
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// AMQP LIB 
+// AMQP LIB
 import amqp from 'amqplib/callback_api';
 const RECEIVE_QUEUE_NAME = "robot_receive_queue";
+const ROBOT_QUEUE_NAME = "to_robot_queue";
 
 const app = require('express')();
 const http = require('http').Server(app);
@@ -8,13 +9,14 @@ const http = require('http').Server(app);
 // Socket io
 const io = require('socket.io')(http);
 
-// Mavlink Parser 
+// Mavlink Parser
 import mavlink from 'mavlink';
 
 // Mavlink instance (0,0) for incoming parser only
-const MAVParser = new mavlink(0,0);
+const MAVParser = new mavlink(0, 0);
 
-app.get('/' , (req, res) => {``
+app.get('/', (req, res) => {
+    ``
     res.send('<h1>hellooo</h1>');
 });
 
@@ -25,63 +27,80 @@ http.listen(3000, () => {
 const display_ip = (sock) => {
     let socketId = sock.request.connection.remotePort;
     let clientIp = sock.request.connection.remoteAddress;
-    let ip = clientIp.substring(clientIp.indexOf(":",3)+1);
+    let ip = clientIp.substring(clientIp.indexOf(":", 3) + 1);
     return `${ip}:${socketId}`;
 };
 
 // Server Script
-MAVParser.on("ready" ,() =>{
+MAVParser.on("ready", () => {
     console.log("[Server] MAVLink Parser init XML complete");
+    io.on('connection', socket => {
+        // PROMPT MESSAGE
+        console.log(`${display_ip(socket)} connected`);
+        // STORE KEY : PAIR SOCKET
 
-    amqp.connect('amqp://localhost',(err,conn)=>{
-        console.log("[Server] AMQP Connected !");
-        conn.createChannel(function(err, ch) {
-            ch.assertQueue(RECEIVE_QUEUE_NAME, {durable: false});
+        // Receive data on 'mavlink' socket channel
+        socket.on('mavlink', data => {
+            MAVParser.parse(data);
+        });
 
-            io.on('connection', socket => {
-                // PROMPT MESSAGE
-                console.log(`${display_ip(socket)} connected`);
-                // STORE KEY : PAIR SOCKET 
+        // When socket disconnect
+        socket.on('disconnect', () => {
+            console.log(`${display_ip(socket)} disconnected`);
+        });
 
-                socket.on('mavlink',data => {
-                    MAVParser.parse(data);
-                });
-
-                socket.on('disconnect', () => {
-                    console.log(`${display_ip(socket)} disconnected`);
-                });
-
-                MAVParser.on('message', message=>{
-                    // After Finishing data parsing 
-                    console.log("ENQUEUE");
-                    ch.sendToQueue(RECEIVE_QUEUE_NAME, message.buffer);
-                });
+        // Consume Routine (Do as fast as possible)
+        getChannel(function (ch) {
+            ch.consume(ROBOT_QUEUE_NAME, (message) => {
+                console.log("CONSUME=============>");
+                // PEEK MSG to See What inside 
+                // Select SOCKET From List 
+                // SOCKET.EMIT
+                socket.emit('from_server', message.content);
             });
         });
+
     });
 });
 
-MAVParser.on("GPS_RAW_INT", function(message, fields) {
-		//console.log(fields);
-	});
+// AMQP Instance Connect 
+let amqp_conn;
+amqp.connect('amqp://localhost', (err, conn) => {
+    console.log("[Server] AMQP RECEIVE QUEUE Connected !");
+    amqp_conn = conn;
+});
 
-MAVParser.on('sequenceError', function(mismatch){
+const getChannel = (callback) => {
+    amqp_conn.createChannel(function (err, ch) {
+        return callback(ch);
+    });
+};
+
+
+MAVParser.on('message', message => {
+    getChannel(function (ch) {
+        ch.assertQueue(RECEIVE_QUEUE_NAME, {
+            durable: false
+        });
+        // After Finishing data parsing
+        console.log("<=============ENQUEUE");
+        ch.sendToQueue(RECEIVE_QUEUE_NAME, message.buffer);
+    });
+});
+
+MAVParser.on("GPS_RAW_INT", function (message, fields) {
+    //console.log(fields);
+});
+
+MAVParser.on('sequenceError', function (mismatch) {
     //console.log("Sequence Error " + mismatch)
 });
 
-MAVParser.on('checksumFail', function(mismatch){
+MAVParser.on('checksumFail', function (mismatch) {
     //console.log("Checksum Error")
 });
 
-        // setInterval(()=>{
-        //     MAVParser.createMessage('HEARTBEAT',{
-        //         'type' : 6,
-        //         'autopilot' : 8,
-        //         'base_mode' : 192,
-        //         'custom_mode' : 0,
-        //         'system_status' : 4,
-        //         'mavlink_version' : 3
-        //     },(message)=>{
-        //         socket.emit('server',message.buffer)
-        //     });
-        // },100);
+// setInterval(()=>{     MAVParser.createMessage('HEARTBEAT',{         'type' :
+// 6,         'autopilot' : 8,         'base_mode' : 192,         'custom_mode'
+// : 0,         'system_status' : 4,         'mavlink_version' : 3
+// },(message)=>{         socket.emit('server',message.buffer)     }); },100);
