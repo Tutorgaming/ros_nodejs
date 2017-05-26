c3f4dde3d64c00cfe9e50af116c6678d2ec674b9
iTUTOR <tutorgaming@gmail.com>
2017-05-26 18:20:19 +0700

    Test New

--- robot.js
+++ robot.js
@@ -8,76 +8,126 @@
 import io from 'socket.io-client'
 import mavlink from 'mavlink'
 import mavlinkMessage from 'mavlink'
-import rosnodejs from './rosnodejs/index.js'
+import rosnodejs from 'rosnodejs'
+import BN from 'bn.js'
 
 // MAVLink Parser (for conversion between ROSMAV - MAV)
 const MAVParser = new mavlink();
-
 // Destination server detail
-const SOCKET_IO_SERVER = 'http://192.168.1.117:3000';
-const socket           = io.connect(SOCKET_IO_SERVER);
+const SOCKET_IO_SERVER = 'http://192.168.1.19:3000';
+const socket = io.connect(SOCKET_IO_SERVER);
 
 // Init ROS Node
 const NODENAME = "robot_communication";
+const ROS_MAVLINK_MSG = "mavros_msgs/Mavlink";
+const ROS_OUTGOING_TOPIC = "/mavlink/to_gcs";
+const ROS_INCOMING_TOPIC = "/mavlink/from_gcs";
 
-const rosNode = rosnodejs.initNode(NODENAME,{onTheFly: true}).then((rosnode_instance)=>{
-    console.log("[ROBOT] Communication Modules node init complete ! ");
-    // Subscribe to topic to be sent to Server
-    rosnode_instance.subscribe('/mavlink/to_gcs','mavros_msgs/Mavlink',(data)=>{
-        // Parse ROS Message to Buffer
-        let msgBuf = new Buffer(data.len + 8);
-        msgBuf.fill('\0');
+var rn;
 
-        // Payload
-        const payload64 = data.payload64.reduce(
-            (last,cur)=>
-                Buffer.concat([last,cur.toBuffer('le',8)])
-        ,new Buffer(0)
-        ); 
-        
-        // Create mavlink buffer
-        msgBuf[0] = data.magic;
-        msgBuf[1] = data.len;
-        msgBuf[2] = data.seq;
-        msgBuf[3] = data.sysid;
-        msgBuf[4] = data.compid;
-        msgBuf[5] = data.msgid;
-        payload64.copy(msgBuf,6,0);
-        msgBuf.writeUInt16LE(data.checksum, data.len+6);
-        
-        socket.emit('mavlink',msgBuf);
+MAVParser.on('ready', () => {
+    console.log("MAVParser Ready");
+    rosnodejs.initNode(NODENAME, {
+        onTheFly: true
+    }).then((rosnode_instance) => {
+        console.log("[ROBOT] Communication Modules node init complete ! ");
 
-    },
-    {
-      queueSize: 1,
-      throttleMs: 200
+        rn = rosnode_instance;
+        // Subscribe to topic to be sent to Server
+        rn.subscribe(ROS_OUTGOING_TOPIC, ROS_MAVLINK_MSG, (data) => {
+            // Parse ROS Message to Buffer
+            let msgBuf = new Buffer(data.len + 8);
+            msgBuf.fill('\0');
+
+            // Payload
+            const payload64 = data.payload64.reduce(
+                (last, cur) =>
+                Buffer.concat([last, cur.toBuffer('le', 8)]), new Buffer(0)
+            );
+
+            // Create mavlink buffer
+            msgBuf[0] = data.magic;
+            msgBuf[1] = data.len;
+            msgBuf[2] = data.seq;
+            msgBuf[3] = data.sysid;
+            msgBuf[4] = data.compid;
+            msgBuf[5] = data.msgid;
+            payload64.copy(msgBuf, 6, 0);
+            msgBuf.writeUInt16LE(data.checksum, data.len + 6);
+
+            socket.emit('mavlink', msgBuf);
+        }, {
+            queueSize: 10,
+            throttleMs: 100
+        });
+
+
+
+        socket.on('connect', () => {
+            console.log("[ROBOT]['connect'] Connected to SOCKET.IO SERVER ");
+        });
     });
 });
 
-MAVParser.on('ready', ()=>{
-    console.log("MAVParser Ready");
-    // // On Receive Server command
-    
-});
-
-socket.on('from_server',(data)=>{
-        // Convert MAVLINK BUFFER TO ROSMSG
+socket.on('from_server', (data) => {
+    // Convert MAVLINK BUFFER TO ROSMSG
+    //console.log(data);
+    if (rn !== null)
         MAVParser.parse(data);
 });
 
-socket.on('connect', ()=>{
-    console.log("[ROBOT]['connect'] Connected to SOCKET.IO SERVER " );
-});
+// On Parse Completed !
+MAVParser.on('message', (message) => {
+    // Create Ros message
+    if (rn === null) return;
+    let byte_offset = 0;
+    let payload_64 = [];
 
-MAVParser.on('message', (message)=>{
-        console.log(message);
-        // PUB TO ROS WORLD
-});
+    // Create Payload 64
+    for (let i = 0; i < Math.ceil(message.length / 8); i++) {
+        // Read Buffer offset 8
+        let remaining_count = (message.length - byte_offset) > 8 ?
+            8 : (message.length - byte_offset);
+        let temp = new Buffer(8);
+        temp.fill(0);
+        message.payload.copy(temp, 0, byte_offset, byte_offset + remaining_count);
+        let bignum = new BN(temp, '8', 'le');
+        payload_64.push(bignum);
+        byte_offset += 8;
+    }
 
-MAVParser.on('sequenceError', function(mismatch){
-    console.log("Sequence Error : " + mismatch)
-});
+    // Define ROS Message TYPE (Under Scope)
+    const mavros_msgs_type = rosnodejs.require('mavros_msgs').msg;
+    const Header = rosnodejs.require('std_msgs').msg.Header;
+    const header = new Header({
+        seq: 100,
+        stamp: { secs: 2245, nsecs: 912000000 },
+        frame_id: ''
+    });
+    console.log("message coming");
 
-MAVParser.on('checksumFail', (mismatch)=>{
-    console.log("Checksum Failed : " + mismatch);
+
+    let pub = rn.advertise('/mavlink/from_gcs', 'mavros_msgs/Mavlink', {
+        queueSize: 10,
+        latching: true,
+        throttleMs: 100
+    });
+
+    // const mav_message = new mavros_msgs_type.Mavlink({
+    //     // // Reuse a message to be sent
+    //     header: header,
+    //     len: message.length,
+    //     seq: message.sequence,
+    //     sysid: message.system,
+    //     compid: message.component,
+    //     msgid: message.id,
+    //     checksum: message.checksum,
+    //     payload64: payload_64
+    // });
+    const mav_message = new mavros_msgs_type.Mavlink();
+
+    console.log(mav_massage);
+    // Publish to ROS
+    pub.publish(mav_message);
+
 });
